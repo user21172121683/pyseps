e6e796a4deaa5783cd08d9fc81204ea16af13e97
user21172121683 <lukas@messing.ee>
2025-09-27 23:02:56 +0300

    Add ProcessSep

--- src/core/sep.py
+++ src/core/sep.py
@@ -11,3 +11,14 @@ class Sep(ABC):
     @abstractmethod
     def split(self):
         pass
+
+
+class ProcessSep(Sep):
+    def split(self):
+        """Split CMYK image into C, M, Y, K channels."""
+        if self.image.mode.upper() != "CMYK":
+            image = self.image.convert("CMYK")
+        else:
+            image = self.image
+        c, m, y, k = image.split()
+        self.separations = {"C": c, "M": m, "Y": y, "K": k}
