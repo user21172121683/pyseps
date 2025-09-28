a9d6dc87f5ef1902892c421a4f0de67ec2c16ea1
user21172121683 <lukas@messing.ee>
2025-09-29 00:30:47 +0300

    Add thumbnail

--- src/utils/helpers.py
+++ src/utils/helpers.py
@@ -1,2 +1,11 @@
+from PIL import Image
+
+
 def norm_intensity(value: int) -> float:
     return max(0.0, min(1.0, value / 255.0))
+
+
+def thumbnail(image: Image, dim: int = 200) -> Image:
+    thumb = image.copy()
+    thumb.thumbnail((dim, dim), Image.Resampling.LANCZOS)
+    return thumb
