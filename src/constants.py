713c2751c9ba3237aa1a939a5692b601a1934e79
user21172121683 <lukas@messing.ee>
2025-09-30 11:50:37 +0300

    Add banner

--- src/constants.py
+++ src/constants.py
@@ -5,3 +5,11 @@ ROOT = Path(__file__).resolve().parents[1]
 TEMPLATES_DIR = ROOT / "templates"
 DEFAULT_TEMPLATE = TEMPLATES_DIR / "default.yaml"
 APP_NAME = "pyseps"
+BANNER = """
++      _/_/_/    _/    _/    _/_/_/    _/_/    _/_/_/      _/_/_/ +
+      _/    _/  _/    _/  _/_/      _/_/_/_/  _/    _/  _/_/       
+     _/    _/  _/    _/      _/_/  _/        _/    _/      _/_/    
+    _/_/_/      _/_/_/  _/_/_/      _/_/_/  _/_/_/    _/_/_/       
+   _/--------------_/----------------------_/-----------------     
++ _/          _/_/                        _/                      +
+"""
