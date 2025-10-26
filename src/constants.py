b492e4a2a4e8c4e761b00a79cc9234988258cb68
user21172121683 <lukas@messing.ee>
2025-10-26 16:12:43 +0200

    Replace magic string

--- src/constants.py
+++ src/constants.py
@@ -4,6 +4,7 @@ from pathlib import Path
 ROOT = Path(__file__).resolve().parents[1]
 TEMPLATES_DIR = ROOT / "src" / "templates"
 DEFAULT_TEMPLATE = TEMPLATES_DIR / "default.yaml"
+DEFAULT_OUTPUT_DIR = "seps"
 APP_NAME = "pyseps"
 BANNER = """
 +      _/_/_/    _/    _/    _/_/_/    _/_/    _/_/_/      _/_/_/ +
