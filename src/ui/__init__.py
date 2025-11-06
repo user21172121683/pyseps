b7a058660e0a6cbfb57b79616396dca4e96a8021
user21172121683 <lukas@messing.ee>
2025-11-06 22:13:13 +0200

    Add GUI placeholder

--- src/ui/__init__.py
+++ src/ui/__init__.py
@@ -1,3 +1,4 @@
 # SPDX-License-Identifier: AGPL-3.0-or-later
 
 from .cli.main import AppCLI
+from .gui.main import AppGUI
---
+++ src/ui/gui/main.py
@@ -0,0 +1,6 @@
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
+
+class AppGUI:
+    def run(self):
+        pass
